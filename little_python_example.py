1563a21e6bf9b9477e82254cc584da23a19305e1
dev-shark <viacheslav.sharkovskii@ya.se>
2023-01-12 14:52:27 +0100

    Update Planning for Friday and Monday
    Update Planning part for Friday and Monday
    Add a little example for python script.

---
+++ little_python_example.py
@@ -0,0 +1,12 @@
+
+my_variable = 2023
+my_second_variable = 100
+
+string_var = "my new text line"
+
+print(my_variable)
+print(my_second_variable)
+
+print(my_variable + my_second_variable)
+
+print(string_var)
